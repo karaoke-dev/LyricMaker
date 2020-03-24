f5093138ea4a1950da020dd190c8412062772af6
andy840119 <andy840119@gmail.com>
2020-03-24 23:15:18 +0900

    Add initial test file

---
+++ LyricMaker.Tests/BaseUnitTest.cs
@@ -0,0 +1,24 @@
+﻿using LyricMaker.Extensions;
+using System.Globalization;
+using NUnit.Framework;
+
+namespace LyricMaker.Tests
+{
+    public class BaseUnitTest
+    {
+        [SetUp]
+        public void Initialize()
+        {
+            // Use [mm:ss:ss]
+            TimeTagExtension.SetDecimalPointColon();
+        }
+
+        protected void IsLyricEqual(string expected, string actual, CultureInfo info = null)
+        {
+            var expectedLyric = expected.Replace("\n", "");
+            var actualLyric = expected.Replace("\n", "");
+
+            Assert.AreEqual(expectedLyric, actualLyric);
+        }
+    }
+}
---
+++ LyricMaker.Tests/Resources/TestResources.cs
@@ -0,0 +1,23 @@
+// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.IO;
+using osu.Framework.IO.Stores;
+
+namespace osu.Game.Rulesets.Karaoke.Tests.Resources
+{
+    public static class TestResources
+    {
+        public static DllResourceStore GetStore() => new DllResourceStore(typeof(TestResources).Assembly);
+
+        public static Stream OpenResource(string name) => GetStore().GetStream($"Resources/{name}");
+
+        public static Stream OpenBeatmapResource(string name) => OpenResource($"Testing/Beatmaps/{name}.osu");
+
+        public static Stream OpenSkinResource(string name) => OpenResource($"Testing/Skin/{name}.skin");
+
+        public static Stream OpenLrcResource(string name) => OpenResource($"Testing/Lrc/{name}.lrc");
+
+        public static Stream OpenNicoKaraResource(string name) => OpenResource($"Testing/NicoKara/{name}.nkmproj");
+    }
+}
