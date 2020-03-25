bac6f3ef6de3e722a4dc1465dc7757770de79de7
andy840119 <andy840119@gmail.com>
2020-03-26 00:17:34 +0900

    Add initial UI testcase and test runner

---
+++ LyricMaker.Tests/Visual/Screens/TestSceneLyricMakerScreen.cs
@@ -0,0 +1,15 @@
+﻿using LyricMaker.UI.Screens;
+using LyricMaker.UI.Tests.Visual;
+using NUnit.Framework;
+
+namespace LyricMaker.Tests.Visual.Screens
+{
+    [TestFixture]
+    public class TestSceneLyricMakerScreen : LyricMakerTestScene
+    {
+        public TestSceneLyricMakerScreen()
+        {
+            Child = new LyricMakerScreen();
+        }
+    }
+}
---
+++ LyricMaker.Tests/VisualTestRunner.cs
@@ -0,0 +1,20 @@
+﻿using LyricMaker.UI.Tests;
+using osu.Framework;
+using osu.Framework.Platform;
+using System;
+
+namespace LyricMaker.Tests
+{
+    public static class VisualTestRunner
+    {
+        [STAThread]
+        public static int Main(string[] args)
+        {
+            using (DesktopGameHost host = Host.GetSuitableHost(@"LyricMaker", true))
+            {
+                host.Run(new LyricMakerTestBrowser());
+                return 0;
+            }
+        }
+    }
+}
